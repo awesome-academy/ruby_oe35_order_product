517e8a51271262204adae4a929ac9ae2df330027
TienTM25 <trinhmanhtienn@gmail.com>
2021-07-05 08:16:04 +0700

    sign_in_admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,3 +1,20 @@
 class Admin::AdminController < ApplicationController
+  before_action :logged_in_user, :admin_user, only: :home
+
   layout "admin/application"
+
+  include LoginSessionHelper
+
+  def home; end
+
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = t "admin.admin_session.warning_text"
+    redirect_to admin_login_url
+  end
+
+  def admin_user
+    redirect_to admin_login_url unless current_user.role?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
     resources :products, only: :index
     resources :users
     namespace :admin do
-      root to: "products#index"
+      root to: "admin#home"
       resources :products
+      get "/login", to: "admin_session#new"
+      post "/login", to: "admin_session#create"
+      delete "/logout", to: "admin_session#destroy"
     end
   end
 end
